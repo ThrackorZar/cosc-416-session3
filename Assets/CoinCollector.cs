0c44fa145c5b84d8f7cd54e389c72d2c32becff8
ThrackorZar <78045041+ThrackorZar@users.noreply.github.com>
2025-03-02 22:40:38 -0800

    Update CoinCollector.cs

--- Assets/CoinCollector.cs
+++ Assets/CoinCollector.cs
@@ -1,1 +1,37 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoinCollector : MonoBehaviour
+{
+    public UnityEvent OnCoinCollected = new();
+    public float pointValue = 1f;
+    private bool isCollected = false;
+
+    [SerializeField] private float rotationSpeed = 100f;
+
+    void Update()
+    {
         // Rotate the coin
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isCollected)
+        {
+            isCollected = true;
+            OnCoinCollected?.Invoke();
+            
+            GameManager gameManager = FindFirstObjectByType<GameManager>();
+            if (gameManager != null)
+            {
+                gameManager.AddScore(pointValue);
+            }
+            
+            gameObject.SetActive(false);
+            Destroy(gameObject, 0.1f);
+            
+            Debug.Log("Coin collected!");
+        }
+    }
+}
